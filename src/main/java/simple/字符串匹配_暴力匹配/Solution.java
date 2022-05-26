a6c91b601b6ff71b79cb26c4a31ee51034542006
zhiheng.wang <zhiheng.wang@thoughtworks.com>
2022-05-26 11:42:28 +0800

    2022 05 26 -2

---
+++ src/main/java/simple/字符串匹配_暴力匹配/Solution.java
@@ -0,0 +1,33 @@
+package simple.字符串匹配_暴力匹配;
+
+/*
+实现 strStr() 函数。
+
+给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回  -1 。
+
+说明：
+
+当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+
+对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与 C 语言的 strstr() 以及 Java 的 indexOf() 定义相符。
+
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/implement-strstr
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() > haystack.length()) return -1;
+        for (int i = 0; i < haystack.length(); i++) {
+            for (int j = 0; j < needle.length(); j++) {
+                if (i + j > haystack.length() - 1 || needle.charAt(j) != haystack.charAt(i + j)) break;
+                if (j == needle.length() - 1) return i;
+            }
+
+        }
+        return -1;
+
+    }
+}
