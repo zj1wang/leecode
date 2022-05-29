04909acc40dc2fc98e515bad01ea41f685dcfa26
zhiheng.wang <zhiheng.wang@thoughtworks.com>
2022-05-29 11:30:46 +0800

    2022 05 29 -3

---
+++ src/main/java/simple/合并两个有序数组/Solution.java
@@ -0,0 +1,35 @@
+package simple.合并两个有序数组;
+
+
+/**
+ * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+ * <p>
+ * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+ * <p>
+ * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/merge-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+//        暴力算法O（nm）
+//                双指针O（n+m）
+//    倒序或者正序采用临时变量
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int tail = nums1.length - 1;
+        int p1 = m - 1;
+        int p2 = n - 1;
+        while (p2 >= 0) {
+            if (p1 < 0 || nums1[p1] <= nums2[p2]) {
+                nums1[tail--] = nums2[p2--];
+            } else {
+                nums1[tail--] = nums1[p1--];
+            }
+        }
+    }
+}
