b3071d5fdf745a558a5207595ca8788fe7d84d41
zhiheng.wang <zhiheng.wang@thoughtworks.com>
2022-05-29 11:12:58 +0800

    2022 05 29 -2

---
+++ src/main/java/simple/删除排序链表中的重复元素/Solution.java
@@ -0,0 +1,40 @@
+package simple.删除排序链表中的重复元素;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+//     给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+//11233
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode cur = head;
+        while (cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return head;
+    }
+}
+
