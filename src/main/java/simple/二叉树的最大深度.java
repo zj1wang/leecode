5e49d08759499d42a24fe55e7c55d67be7d82bf7
zhiheng.wang <zhiheng.wang@thoughtworks.com>
2022-06-13 20:23:41 +0800

    2022 06 13 -1：深度优先遍历

---
+++ src/main/java/simple/二叉树的最大深度.java
@@ -0,0 +1,44 @@
+package simple;
+
+/*
+* 给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+*
+*
+* */
+
+public class 二叉树的最大深度 {
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+
+        }
+    }
+}
